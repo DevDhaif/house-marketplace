48c021d63f156ccd396e22c924f12c06e26e1a09
DevDhaif <devdhaif@gmail.com>
2022-02-26 01:53:07 +0200

    added logout

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,26 @@
 import {useEffect,useState} from 'react'
 import {getAuth} from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 function Profile() {
-  const [user,setUser]=useState(null)
   const auth =getAuth()
-  useEffect(()=>{
-    setUser(auth.currentUser)
-  },[])
-  return user?(
-    <h1>Hi {user.displayName}</h1>
-  ):(
-    <h1>Guest</h1>
+  const [formData,setFormData]=useState({
+    name:auth.currentUser.displayName,
+    email:auth.currentUser.email
+  })
+
+  const {name,email}=formData
+  const navigate=useNavigate()
+  const onLogOut=()=>{
+    auth.signOut()
+    navigate('/')
+  }
+  return (
+    <div className=''>
+      <header className='flex justify-between mx-4 my-2'>
+        <h1 className='text-xl font-semibold'>My Profile</h1>
+        <button className='px-2 py-1 bg-green-500 font-semibold text-white rounded-xl' onClick={onLogOut}>Log out</button>
+      </header>
+    </div>
   )
 }
 
